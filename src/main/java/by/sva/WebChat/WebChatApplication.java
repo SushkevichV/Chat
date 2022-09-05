b89453e93cb8d6a5c60f0a3cfc1725fd74ba5a8e
Vladislav Sushkevich <71271188+SushkevichV@users.noreply.github.com>
2022-09-05 19:11:53 +0300

    Add files via upload

---
+++ src/main/java/by/sva/WebChat/WebChatApplication.java
@@ -0,0 +1,17 @@
+package by.sva.WebChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Чат
+
+// ссылка на видео https://www.youtube.com/watch?v=o07DfTMs2N4
+
+@SpringBootApplication
+public class WebChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatApplication.class, args);
+	}
+
+}
